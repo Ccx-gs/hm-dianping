f472cb79c379090d3338bc4cf1e47ad895275fb1
Ccx-gs <2737106950@qq.com>
2026-03-25 20:38:24 +0800

    完善用户登录功能

--- src/main/java/com/hmdp/utils/UserHolder.java
+++ src/main/java/com/hmdp/utils/UserHolder.java
@@ -3,12 +3,15 @@ package com.hmdp.utils;
 import com.hmdp.dto.UserDTO;
 
 public class UserHolder {
+    // 1. 泛型改成 UserDTO
     private static final ThreadLocal<UserDTO> tl = new ThreadLocal<>();
 
+    // 2. 参数改成 UserDTO
     public static void saveUser(UserDTO user){
         tl.set(user);
     }
 
+    // 3. 返回值改成 UserDTO
     public static UserDTO getUser(){
         return tl.get();
     }
